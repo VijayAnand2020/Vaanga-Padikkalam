a969abdbb914983ed77f9d3f5a1d910c4a0c5c3a
VijayAnand2020 <sankariraja28@gmail.com>
2020-08-20 22:27:25 +0530

    Commit of Feedback,Models,Admin on 20-08-2020 22:26
    Co-authored-by: RAAMSRI2020 <sriraam210903@gmail.com>

--- home/models.py
+++ home/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Feedback(models.Model):
+    model_feedback_name = models.CharField(max_length=30, default='Name not provided')
     model_feedback_rating = models.CharField(max_length=10)
     model_feedback_text = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.model_feedback_rating}, {self.model_feedback_text}"
-
+        return f"{self.model_feedback_name}: Rating:{self.model_feedback_rating}, Feedback_Message:{self.model_feedback_text}"
--- home/views.py
+++ home/views.py
@@ -5,14 +5,11 @@ from .models import Feedback
 def home(request):
     response_rating = request.POST.get('radioset')
     response_text = request.POST.get('feedback_text')
-    if response_rating != None or response_text != None:
-        MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text=response_text)
-        # MyFeedback.save()
-        # print(response_rating,response_text)
-    elif response_text == None and response_rating != None:
-        MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text="No Response Text")
-        # MyFeedback.save()
-    elif response_text != None and response_rating == None:
+    response_name = request.POST.get('feedback_name')
+    if response_rating != None and response_text != None and response_name != None:
+        MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text=response_text, model_feedback_name=response_name)
+    elif response_text == None and response_rating != None and response_name != None:
+        MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text="No Response Text", model_feedback_name=response_name)
+    elif response_text != None and response_rating == None and response_name==None:
         MyFeedback = Feedback.objects.create(model_feedback_rating="No Response Rating",model_feedback_text=response_text)
-        # MyFeedback.save()
     return render(request, 'index.html')
