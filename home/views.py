c79b33b8e1438ce6147a4ba4232c871886c24990
VijayAnand2020 <sankariraja28@gmail.com>
2020-08-20 12:34:41 +0530

    Commit on 20-08-2020 12:34 Duration_of_work:33mins

--- home/views.py
+++ home/views.py
@@ -6,10 +6,13 @@ def home(request):
     response_rating = request.POST.get('radioset')
     response_text = request.POST.get('feedback_text')
     if response_rating != None or response_text != None:
-        MyFeedback = Feedback(response_rating,response_text)
+        MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text=response_text)
+        # MyFeedback.save()
         # print(response_rating,response_text)
     elif response_text == None and response_rating != None:
-        MyFeedback = Feedback(response_rating)
+        MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text="No Response Text")
+        # MyFeedback.save()
     elif response_text != None and response_rating == None:
-        MyFeedback = Feedback(response_text)
+        MyFeedback = Feedback.objects.create(model_feedback_rating="No Response Rating",model_feedback_text=response_text)
+        # MyFeedback.save()
     return render(request, 'index.html')
