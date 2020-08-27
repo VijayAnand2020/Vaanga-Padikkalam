42891d88526289c95e7322f3e77579c81c2fd8a1
VijayAnand2020 <sankariraja28@gmail.com>
2020-08-27 22:38:23 +0530

    Changed Mobile views.Feedback backend-Flop
    Co-authored-by: RAAMSRI2020 <sriraam210903@gmail.com>

--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import Feedback
 
 # Create your views here.
-def home(request):
+def home(request): 
+    return render(request, 'index.html')
+
+def feedback(request):
     response_rating = request.POST.get('radioset')
     response_text = request.POST.get('feedback_text')
     response_name = request.POST.get('feedback_name')
@@ -12,7 +15,6 @@ def home(request):
         MyFeedback = Feedback.objects.create(model_feedback_rating=response_rating,model_feedback_text="No Response Text", model_feedback_name=response_name)
     elif response_text != None and response_rating == None and response_name==None:
         MyFeedback = Feedback.objects.create(model_feedback_rating="No Response Rating",model_feedback_text=response_text)
-    return render(request, 'index.html')
-
-def feedback(request):
+    else:
+        MyFeedback = Feedback.objects.create(model_feedback_rating="No Response Rating",model_feedback_text="No Response Text")
     return render(request, 'home/feedback_form.html')
