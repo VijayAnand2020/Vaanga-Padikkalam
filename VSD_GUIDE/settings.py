175a30aab62f0efa1da64479a5213047b0c788f5
VijayAnand2020 <sankariraja28@gmail.com>
2020-09-06 21:41:59 +0530

    Added Static Root

--- VSD_GUIDE/settings.py
+++ VSD_GUIDE/settings.py
@@ -131,5 +131,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
